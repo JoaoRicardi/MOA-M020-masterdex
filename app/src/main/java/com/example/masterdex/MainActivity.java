c9f7e5a4039194163ab436a9bd4488e96968130f
Erik Antunes <erik.antunes@live.com>
2019-06-29 10:46:17 -0300

    TabLayout incompleto

--- app/src/main/java/com/example/masterdex/MainActivity.java
+++ app/src/main/java/com/example/masterdex/MainActivity.java
@@ -1,45 +1,53 @@
 package com.example.masterdex;
 
-import android.content.Intent;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 import com.example.masterdex.adapter.ViewPagerAdapter;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
     private AppBarLayout appBarLayout;
     private ViewPager viewPager;
+    private int[] tabIcons = {
+            R.drawable.ic_facebook};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       // Intent intent = new Intent(getApplicationContext(),PokemonsActivity.class);
-       // startActivity(intent);
-       // finish();
 
 
         tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
         appBarLayout = (AppBarLayout) findViewById(R.id.appbar_id);
         viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+
+
+
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
 
-        //Adicionando as Activitys
-        adapter.AddFragment(new PokemonsFragment(),"Dex");
-        adapter.AddFragment(new PerfilFragment(),"Perfil");
-        adapter.AddFragment(new RegionsFragment(),"Regiões");
+        //Adicionando os Fragments
+        adapter.AddFragment(new PokemonsFragment(), "Dex");
+        adapter.AddFragment(new PerfilFragment(), "Perfil");
+        adapter.AddFragment(new RegionsFragment(), "Regiões");
+
 
         // Adapter Setup
 
         viewPager.setAdapter(adapter);
-        tabLayout.setupWithViewPager(viewPager);
-
-
     }
+
+
+
 }
