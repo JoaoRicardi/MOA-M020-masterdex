85c8a612c921767f1957f944bd37e656e2999f12
Erik Antunes <erik.antunes@live.com>
2019-06-29 13:29:44 -0300

    ajuste na cor dos Tabs

--- app/src/main/java/com/example/masterdex/MainActivity.java
+++ app/src/main/java/com/example/masterdex/MainActivity.java
@@ -1,19 +1,18 @@
 package com.example.masterdex;
 
-import android.support.design.widget.AppBarLayout;
+
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+
 
 import com.example.masterdex.adapter.ViewPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+
     private TabLayout tabLayout;
-    private AppBarLayout appBarLayout;
     private ViewPager viewPager;
     private int[] tabIcons = {
             R.drawable.ic_facebook};
@@ -26,15 +25,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = findViewById(R.id.toolbar_tab_menu);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
         viewPager = findViewById(R.id.viewpager_id);
         setupViewPager(viewPager);
 
-        tabLayout = findViewById(R.id.tablayout_id);
+        tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
     }
