c7c94feaa8549a2cb9d9833c29bb0bdd4ddbb41c
Erik Antunes <erik.antunes@live.com>
2019-06-29 12:49:25 -0300

    parou de crashar hehe

--- app/src/main/java/com/example/masterdex/MainActivity.java
+++ app/src/main/java/com/example/masterdex/MainActivity.java
@@ -5,49 +5,49 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.widget.Toolbar;
 
 import com.example.masterdex.adapter.ViewPagerAdapter;
 
-
-
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     private TabLayout tabLayout;
     private AppBarLayout appBarLayout;
     private ViewPager viewPager;
     private int[] tabIcons = {
             R.drawable.ic_facebook};
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = findViewById(R.id.toolbar_tab_menu);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-        tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
-        appBarLayout = (AppBarLayout) findViewById(R.id.appbar_id);
-        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+        viewPager = findViewById(R.id.viewpager_id);
+        setupViewPager(viewPager);
 
+        tabLayout = findViewById(R.id.tablayout_id);
         tabLayout.setupWithViewPager(viewPager);
 
-
-
-
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-
-        //Adicionando os Fragments
-        adapter.AddFragment(new PokemonsFragment(), "Dex");
-        adapter.AddFragment(new PerfilFragment(), "Perfil");
-        adapter.AddFragment(new RegionsFragment(), "Regiões");
-
-
-        // Adapter Setup
-
-        viewPager.setAdapter(adapter);
     }
 
-
-
+        private void setupViewPager(ViewPager viewPager) {
+            ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+            adapter.AddFragment(new PokemonsFragment(), "Dex");
+            adapter.AddFragment(new PerfilFragment(), "Perfil");
+            adapter.AddFragment(new RegionsFragment(), "Regiões");
+            viewPager.setAdapter(adapter);
+        }
 }
+
+
+
+
