c57dec04529b48d0a7f7938d1d95dc6de91f95f0
dontsaymyname <michaelpinheiro00@gmail.com>
2019-06-30 12:24:34 -0300

    perfil base

--- app/src/main/java/com/example/masterdex/PerfilFragment.java
+++ app/src/main/java/com/example/masterdex/PerfilFragment.java
@@ -4,13 +4,17 @@ package com.example.masterdex;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.PopupMenu;
 
 
 public class PerfilFragment extends Fragment {
 
+    Button buttonOpcoesPerfil;
 
     public PerfilFragment() {
         // Required empty public constructor
@@ -23,7 +27,19 @@ public class PerfilFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_perfil, container, false);
 
-        //trabalhe aqui dentro
+        buttonOpcoesPerfil = view.findViewById(R.id.button_opcoes_perfil);
+        buttonOpcoesPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                PopupMenu popupMenu = new PopupMenu(getActivity(), buttonOpcoesPerfil);
+                MenuInflater menuInflater = popupMenu.getMenuInflater();
+                menuInflater.inflate(R.menu.popup_menu, popupMenu.getMenu());
+                popupMenu.show();
+
+
+            }
+        });
 
         return view;
     }
