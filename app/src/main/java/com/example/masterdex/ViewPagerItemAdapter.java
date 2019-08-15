e777ceb20f68a127a50d895c08b68ddde1ab8dee
Paulo Henrique <neomatrixgames@gmail.com>
2019-08-14 21:34:15 -0300

    uns baratos loucos

---
+++ app/src/main/java/com/example/masterdex/ViewPagerItemAdapter.java
@@ -0,0 +1,42 @@
+package com.example.masterdex;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> fragmentList = new ArrayList();
+    private final List<String> FragmentListTitles = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return FragmentListTitles.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return FragmentListTitles.get(position);
+    }
+
+    public  void AddFragment (Fragment fragment, String Tittle){
+
+        fragmentList.add(fragment);
+        FragmentListTitles.add(Tittle);
+
+    }
+}
