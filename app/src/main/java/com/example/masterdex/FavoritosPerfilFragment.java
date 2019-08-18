ef7fc723022feca0ed206c8e8aa64d5b3106c156
Erik Antunes <erik.antunes@live.com>
2019-08-18 20:39:57 -0300

    colocando os Pokemons na tela

--- app/src/main/java/com/example/masterdex/FavoritosPerfilFragment.java
+++ app/src/main/java/com/example/masterdex/FavoritosPerfilFragment.java
@@ -8,15 +8,22 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.masterdex.adapter.AdapterPerfilFavoritos;
+
 
 public class FavoritosPerfilFragment extends Fragment {
 
-    View view;
+    private RecyclerView favoritosRecyclerView;
+    private AdapterPerfilFavoritos favoritosAdapter;
+    private static final int LIMIT = 20;
+    private int offset = 0;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view =inflater.inflate(R.layout.fragment_favoritos_perfil,container,false);
+        View view =inflater.inflate(R.layout.fragment_favoritos_perfil,container,false);
         return view;
 
 
---
+++ app/src/main/java/com/example/masterdex/adapter/AdapterPerfilFavoritos.java
@@ -0,0 +1,122 @@
+package com.example.masterdex.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.masterdex.R;
+import com.example.masterdex.models.Pokemon;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdapterPerfilFavoritos extends RecyclerView.Adapter<AdapterPerfilFavoritos.ViewHolder> implements Filterable {
+
+    private List<Pokemon> pokemonFilteredList;
+    private List<Pokemon> pokemondListFull;
+
+    public AdapterPerfilFavoritos(List<Pokemon> pokemonFilteredList, List<Pokemon> pokemondListFull) {
+
+        this.pokemonFilteredList = pokemonFilteredList;
+        this.pokemondListFull = pokemondListFull;
+
+        this.pokemonFilteredList = new ArrayList<>(pokemondListFull);
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.pokemons_celula,viewGroup,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+
+        final Pokemon pokemon = pokemonFilteredList.get(position);
+
+        String pok = pokemon.getName();
+        pok = pok.substring(0, 1).toUpperCase().concat(pok.substring(1));
+
+        viewHolder.textNomePokemon.setText(pok);
+
+
+        Picasso.get().load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+pokemon.getNumber()+".png")
+                .into(viewHolder.imageFotoPokemon);
+    }
+
+    @Override
+    public int getItemCount() {
+        return pokemonFilteredList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return pokemonFilter;
+    }
+
+    private Filter pokemonFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<Pokemon> filteredList = new ArrayList<>();
+
+            if (constraint == null || constraint.length() == 0) {
+
+                filteredList.addAll(pokemondListFull);
+
+            } else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+
+                for (Pokemon pokemon : pokemondListFull) {
+                    if (pokemon.getName().toLowerCase().contains(filterPattern)) {
+                        filteredList.add(pokemon);
+                    }
+                }
+            }
+
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            pokemonFilteredList.clear();
+            pokemonFilteredList.addAll((List) results.values);
+            notifyDataSetChanged();
+
+        }
+    };
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textNomePokemon;
+        private ImageView imageFotoPokemon;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textNomePokemon = itemView.findViewById(R.id.textNomePokemon);
+            imageFotoPokemon = itemView.findViewById(R.id.imageFotoPokemon);
+
+        }
+    }
+
+    public void atualizarFavoritosPerfil(List<Pokemon> favoritosListPokemon){
+        pokemondListFull = favoritosListPokemon;
+        pokemonFilteredList.addAll(pokemondListFull);
+        notifyDataSetChanged();
+    }
+}
