38f2b1e45b22663f84249b5e24dbb990c3c48c35
Erik Antunes <erik.antunes@live.com>
2019-08-18 18:54:15 -0300

    fragments

---
+++ app/src/main/java/com/example/masterdex/CapturadosPerfilFragment.java
@@ -0,0 +1,23 @@
+package com.example.masterdex;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CapturadosPerfilFragment extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_capturados_perfil,container,false);
+        return view;
+        
+    }
+}
