806328afc05c0fb118bbab94d904ccc6a8344b2c
Paulo Henrique <neomatrixgames@gmail.com>
2019-08-18 11:55:07 -0300

    agora a tela de recuperar senha funciona \o/

--- app/src/main/java/com/example/masterdex/view/EsqueciASenha.java
+++ app/src/main/java/com/example/masterdex/view/EsqueciASenha.java
@@ -3,38 +3,43 @@ package com.example.masterdex.view;
 import android.content.Intent;
 
 import com.example.masterdex.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class EsqueciASenha extends AppCompatActivity {
 
     private TextInputEditText emailDigitado;
     private ImageView buttonVoltar;
     private Button buttonEnviar;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_esqueci_asenha);
 
+
+        auth = FirebaseAuth.getInstance();
         emailDigitado = findViewById(R.id.email_textinput);
         buttonVoltar = findViewById(R.id.botao_voltar_esqueci_senha);
         buttonEnviar = findViewById(R.id.enviar_button);
 
 
-        buttonEnviar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                enviarClicado(view);
-            }
-        });
+        buttonEnviar.setOnClickListener(this::enviarClicado);
 
         buttonVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,18 +51,34 @@ public class EsqueciASenha extends AppCompatActivity {
 
     }
 
+    private void trocarSenha() {
+        auth.sendPasswordResetEmail(Objects.requireNonNull(emailDigitado.getText())
+                .toString().toLowerCase())
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(EsqueciASenha.this,
+                                "Acesse seu email, e siga os passos para mudar a Senha.",
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(EsqueciASenha.this,
+                                Objects.requireNonNull(task.getException()).getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
 
     public void enviarClicado(View view) {
 
         android.view.inputmethod.InputMethodManager teclado = (android.view.inputmethod.InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
 
         if (teclado.isAcceptingText()) {
-            teclado.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+            teclado.hideSoftInputFromWindow(Objects.requireNonNull(getCurrentFocus()).getWindowToken(), 0);
         }
         //Em caso de teclado visível, este método esconde ele
 
 
-        if (emailDigitado.getText().toString().equals("")) {
+        if (Objects.requireNonNull(emailDigitado.getText()).toString().equals("")) {
             //Se não houver texto no campo do email, ele irá exibir o erro abaixo
 
             emailDigitado.setError("Digite seu email");
@@ -66,15 +87,7 @@ public class EsqueciASenha extends AppCompatActivity {
             //Caso o campo de email tenha algo, aparecerá um snackbar dizendo que o usuário foi cadastrado
             //Clicar no "ok" do snackbar leva de volta para a tela de login
 
-            Snackbar.make(view, "Email enviado", Snackbar.LENGTH_INDEFINITE)
-                    .setAction("OK", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-
-                            voltarParaLogin();
-
-                        }
-                    }).setActionTextColor(getResources().getColor(R.color.branco)).show();
+            trocarSenha();
 
         }
 
