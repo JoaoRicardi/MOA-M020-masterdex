8f6a59de28657dd71f75a0d7260eff75da66b5ed
dontsaymyname <michaelpinheiro00@gmail.com>
2019-08-20 18:11:19 -0300

    Detalhes fase 2: stats + habilidades

---
+++ app/src/main/java/com/example/masterdex/models/SlotHabilidade.java
@@ -0,0 +1,14 @@
+package com.example.masterdex.models;
+
+public class SlotHabilidade {
+
+    private Habilidade move;
+
+    public Habilidade getMove() {
+        return move;
+    }
+
+    public void setMove(Habilidade move) {
+        this.move = move;
+    }
+}
