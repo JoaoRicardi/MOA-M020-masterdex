2797a5fae82706c09f90fafd51305b4f3fa482bd
Erik Antunes <erik.antunes@live.com>
2019-08-26 00:33:08 -0300

    ajustando

--- app/src/main/java/com/example/masterdex/adapter/PerfilViewPagerAdapter.java
+++ app/src/main/java/com/example/masterdex/adapter/PerfilViewPagerAdapter.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
